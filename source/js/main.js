a3c312ffbfeca34dbb2f0256ac7b3876d4ae238f
Ilshat <ilshat98981@gmail.com>
2022-08-26 15:11:59 +0500

    adds basic markup

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,8 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-
-// ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
-
-  iosVhFix();
 
   // Modules
   // ---------------------------------
@@ -16,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+
   });
 });
 
